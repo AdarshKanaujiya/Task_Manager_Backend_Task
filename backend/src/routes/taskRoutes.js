3421f6c5b7d45ea0db7e5684f7590c1cca8e28a4
AdarshKanaujiya <adarkanjiya@gmail.com>
2026-01-01 15:48:23 +0530

    validator for update task

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import {authMiddleware} from '../middleware/authMiddleware.js';
 import validationMiddleware from '../middleware/validationMiddleware.js';
-import { taskCreationValidator } from '../validators/taskValidators.js';
+import { taskCreationValidator,taskUpdateValidator } from '../validators/taskValidators.js';
 import { me, createTask, getTask, updateTask, deleteTask } from '../controllers/taskController.js';
 
 const routes=express.Router();
@@ -10,7 +10,7 @@ const routes=express.Router();
 routes.get('/me',authMiddleware,me);
 routes.post('/create-task',authMiddleware,taskCreationValidator,validationMiddleware,createTask);
 routes.get('/get-tasks',authMiddleware,getTask);
-routes.put('/update-task/:id',authMiddleware,updateTask);
+routes.put('/update-task/:id',authMiddleware,taskUpdateValidator,validationMiddleware,updateTask);
 routes.delete('/delete-task/:id',authMiddleware,deleteTask);
 
 export default routes;
--- backend/src/validators/taskValidators.js
+++ backend/src/validators/taskValidators.js
@@ -15,3 +15,19 @@ export const taskCreationValidator = [
     .withMessage("Description must be a string"),
 ];
 
+export const taskUpdateValidator = [
+  body("title")
+    .optional()
+    .isLength({ max: 20 })
+    .withMessage("Title can be maximum 20 characters long"),
+  body("description")
+    .optional()
+    .isLength({ max: 200 })
+    .withMessage("Description can be maximum 200 characters long")
+    .isString()
+    .withMessage("Description must be a string"),
+  body("status")
+    .optional()
+    .isIn(["pending", "in-progress", "completed"])
+    .withMessage("Status must be one of: pending, in-progress, completed"),
+];
